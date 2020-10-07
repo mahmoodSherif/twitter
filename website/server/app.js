2a50524223062fbae53d2b3050287f6ddc3645cb
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-07 09:55:48 +0200

    set up cors

--- website/server/app.js
+++ website/server/app.js
@@ -1,22 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var logger = require('morgan');
-const routers = require("./routers");
-var app = express();
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const routers = require('./routers');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/', routers);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
