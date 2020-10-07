dc3302354d47c533ed1d0a0a33e54e13d0d61c65
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-07 09:57:15 +0200

    add tweet list component

---
+++ website/client/src/components/TweetList/TweetList.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Tweet from '../Tweet/Tweet';
+import './tweetList.css';
+
+export default function TweetList() {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const ret = await axios({
+        method: 'get',
+        url: 'http://localhost:3000/feed/',
+        headers: { },
+      });
+      setTweets(ret.data);
+    })();
+  }, []);
+  return (
+      <ul>
+          {tweets.map((tweet) => (<li key={tweet.id}>
+                  <Tweet tweet={tweet} />
+                  </li>))}
+    </ul>
+  );
+}
