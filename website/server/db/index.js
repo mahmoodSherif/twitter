976bcb1550810c126a6a3d3e32a85c7ef4203f6d
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-09 09:38:41 +0200

    add connection info

--- website/server/db/index.js
+++ website/server/db/index.js
@@ -1,6 +1,10 @@
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
