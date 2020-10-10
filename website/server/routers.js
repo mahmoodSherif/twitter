35cc1d96d2f1fba552e53cc33380eb9ebde8dfa7
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-10 08:35:09 +0200

    add users router to the main router

--- website/server/routers.js
+++ website/server/routers.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { feed } = require('./controllers/tweets');
+const userRouter = require('./routers/users');
 
 router.use('/feed/', feed);
-
+router.use(userRouter);
 module.exports = router;
