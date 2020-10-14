9469e68c0958162b282894c1019c7c677994a014
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-14 13:52:08 +0200

    add auth context

--- website/client/src/index.js
+++ website/client/src/index.js
@@ -4,29 +4,39 @@ import {
   BrowserRouter as Router, Link, Route, Switch,
 } from 'react-router-dom';
 import App from './App';
+import SignIn from './components/SignIn/signIn';
+import AuthProvider from './contexts/auth';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/feed">feed</Link>
-          </li>
-          <li>
-            <Link to="/profile">profile</Link>
-          </li>
-        </ul>
-      </div>
-      <Switch>
-        <Route exact path="/feed">
-          <App />
-        </Route>
-        <Route exact path="/profile">
+    <AuthProvider>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/feed">feed</Link>
+            </li>
+            <li>
+              <Link to="/profile">profile</Link>
+            </li>
+            <li>
+              <Link to="/signIn">signIn</Link>
+            </li>
+          </ul>
+        </div>
+        <Switch>
+          <Route exact path="/feed">
+            <App />
+          </Route>
+          <Route exact path="/profile">
 
-        </Route>
-      </Switch>
-    </Router>
+          </Route>
+          <Route exact path="/signIn">
+            <SignIn />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
