7d2f55f77f7cec2b3c11bb73fa2e6f3edf1608f3
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-14 17:05:51 +0200

    format

--- website/client/src/components/TweetForm/TweetForm.js
+++ website/client/src/components/TweetForm/TweetForm.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+
+function addTweet(tweet) {
+  return axios({
+    method: "post",
+    url: "http://localhost:3000/tweets/",
+    headers: { "Content-Type": "application/json" },
+    data: {
+      ...tweet,
+    },
+  });
+}
 
 export default function TweetForm() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [valid, setValid] = useState(true);
   function textChangeHandle(e) {
     if (e.target.value.length < 280) {
@@ -13,14 +25,19 @@ export default function TweetForm() {
   }
   function submitHandle(e) {
     e.preventDefault();
-    setText('');
+    setText("");
   }
   return (
-        <form onSubmit={submitHandle}>
-          <input style={{
-            color: valid ? undefined : 'red',
-          }} type="text" value={text} onChange={textChangeHandle} />
-            <input type="submit" />
-        </form>
+    <form onSubmit={submitHandle}>
+      <input
+        style={{
+          color: valid ? undefined : "red",
+        }}
+        type="text"
+        value={text}
+        onChange={textChangeHandle}
+      />
+      <input type="submit" />
+    </form>
   );
 }
--- website/client/src/components/TweetList/TweetList.js
+++ website/client/src/components/TweetList/TweetList.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Tweet from '../Tweet/Tweet';
-import './tweetList.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Tweet from "../Tweet/Tweet";
+import "./tweetList.css";
 
 export default function TweetList() {
   const [tweets, setTweets] = useState([]);
@@ -9,18 +9,20 @@ export default function TweetList() {
   useEffect(() => {
     (async () => {
       const ret = await axios({
-        method: 'get',
-        url: 'http://localhost:3000/feed/',
-        headers: { },
+        method: "get",
+        url: "http://localhost:3000/feed/",
+        headers: {},
       });
       setTweets(ret.data);
     })();
   }, []);
   return (
-      <ul>
-          {tweets.map((tweet) => (<li key={tweet.id}>
-                  <Tweet tweet={tweet} />
-                  </li>))}
+    <ul>
+      {tweets.map((tweet) => (
+        <li key={tweet.id}>
+          <Tweet tweet={tweet} />
+        </li>
+      ))}
     </ul>
   );
 }
