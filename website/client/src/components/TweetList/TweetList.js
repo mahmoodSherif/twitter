dab58935af85e7eb90bd9099067404cb6c694107
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-15 09:34:22 +0200

    add url

--- website/client/src/components/TweetList/TweetList.js
+++ website/client/src/components/TweetList/TweetList.js
@@ -1,21 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import Tweet from "../Tweet/Tweet";
 import "./tweetList.css";
+import { AuthContext } from "../../contexts/auth";
 
-export default function TweetList() {
+export default function TweetList(props) {
   const [tweets, setTweets] = useState([]);
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     (async () => {
       const ret = await axios({
         method: "get",
-        url: "http://localhost:3000/feed/",
-        headers: {},
+        url: "http://localhost:3000" + props.url,
+        headers: {
+          Authorization: "Bearer " + currentUser.token,
+        },
       });
+      console.log(ret.request);
       setTweets(ret.data);
     })();
-  }, []);
+  }, [currentUser.token, props.url]);
   return (
     <ul>
       {tweets.map((tweet) => (
