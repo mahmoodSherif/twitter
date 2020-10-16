fc31e4122eb636b803418c3b63316122dd254a7a
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-16 10:08:50 +0200

    add follower endpoint

--- website/server/routers/users.js
+++ website/server/routers/users.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
 const passport = require('passport');
 const {
-  findUserById, createUser, follow, unfollow, login,
+  findUserById, createUser, follow, unfollow, login, followers,
 } = require('../controllers/users');
 
 router.post('/login/', login);
 router.post('/users/', createUser);
 
 router.use(passport.authenticate('jwt', { session: false }));
-router.get('/users/:userId/', passport.authenticate('local', { session: false }), findUserById);
-router.post('/users/follow', follow);
-router.delete('/users/follow', unfollow);
+router.get('/users/:userId/', findUserById);
+
+router.get('/users/:userId/followers', followers);
+router.post('/users/:userId/followers', follow);
+router.delete('/users/:userId/followers', unfollow);
 
 module.exports = router;
