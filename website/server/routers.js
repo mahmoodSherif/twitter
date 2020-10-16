c4295c0d2a7a7a19452ce9524a6edf36c30afcfa
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-16 10:09:58 +0200

    move feed endpoint

--- website/server/routers.js
+++ website/server/routers.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const { feed } = require('./controllers/tweets');
 const userRouter = require('./routers/users');
 const tweetRouter = require('./routers/tweets');
 
-router.use('/feed/', feed);
 router.use(userRouter);
 router.use(tweetRouter);
 module.exports = router;
