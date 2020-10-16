633ea20706c7ab2ebf5e6c08f3f6996a65f077cf
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-16 14:49:23 +0200

    move fetch tweets outside tweetList

--- website/client/src/components/TweetList/TweetList.js
+++ website/client/src/components/TweetList/TweetList.js
@@ -1,29 +1,11 @@
-import React, { useContext, useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import Tweet from "../Tweet/Tweet";
 import "./tweetList.css";
-import { AuthContext } from "../../contexts/auth";
 
 export default function TweetList(props) {
-  const [tweets, setTweets] = useState([]);
-  const { currentUser } = useContext(AuthContext);
-
-  useEffect(() => {
-    (async () => {
-      const ret = await axios({
-        method: "get",
-        url: "http://localhost:3000" + props.url,
-        headers: {
-          Authorization: "Bearer " + currentUser.token,
-        },
-      });
-      console.log(ret.request);
-      setTweets(ret.data);
-    })();
-  }, [currentUser.token, props.url]);
   return (
     <ul>
-      {tweets.map((tweet) => (
+      {props.tweets.map((tweet) => (
         <li key={tweet.id}>
           <Tweet tweet={tweet} />
         </li>
