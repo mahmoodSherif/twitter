3d259d06a08a5bc3f256a97a6178b2004b989751
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-26 16:52:49 +0200

    fix the key issue

--- website/client/src/components/TweetList/TweetList.js
+++ website/client/src/components/TweetList/TweetList.js
@@ -5,9 +5,9 @@ import "./tweetList.css";
 export default function TweetList(props) {
   return (
     <ul>
-      {props.tweets.map((tweet) => (
-        <li key={tweet.id}>
-          <Tweet tweet={tweet} />
+      {props.tweets.map((tweetObj) => (
+        <li key={tweetObj.tweet.id}>
+          <Tweet tweet={tweetObj.tweet} user={tweetObj.user} />
         </li>
       ))}
     </ul>
