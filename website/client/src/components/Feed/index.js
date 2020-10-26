d693e8dd13d68e130ecac42d5a4a3a6252e4fb95
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-26 16:53:56 +0200

    fetch the tweets in the feed

--- website/client/src/components/Feed/index.js
+++ website/client/src/components/Feed/index.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useFeed } from "../../actions/tweets";
 import TweetForm from "../TweetForm/TweetForm";
 import TweetList from "../TweetList/TweetList";
 
 export default function Feed() {
+  const tweets = useFeed();
   return (
     <div>
       <TweetForm />
-      <TweetList url="/feed/" />
+      {tweets ? <TweetList tweets={tweets} /> : <h1>Loading...</h1>}
     </div>
   );
 }
