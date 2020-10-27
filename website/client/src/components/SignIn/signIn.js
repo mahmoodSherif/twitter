ecb95a13ae2c6ca2415fc8ecd5bbea6cbde4adbb
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-27 17:20:43 +0200

    add style

--- website/client/src/components/SignIn/signIn.js
+++ website/client/src/components/SignIn/signIn.js
@@ -1,6 +1,16 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 import { AuthContext } from "../../contexts/auth";
+import {
+  Button,
+  TextField,
+  Grid,
+  Paper,
+  makeStyles,
+  Avatar,
+  Typography,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
 async function fetch(username, password) {
   const ret = await axios({
@@ -20,10 +30,25 @@ async function fetch(username, password) {
   }
 }
 
+const useStyles = makeStyles({
+  textField: {
+    margin: "10px",
+  },
+  root: {
+    height: "500px",
+  },
+  button: {},
+  logInPaper: {
+    padding: "10px",
+  },
+});
+
 export default function SignIn() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { currentUser, setCurrentUser } = useContext(AuthContext);
+
+  const classes = useStyles();
 
   function onUsernameChange(e) {
     setUsername(e.target.value);
@@ -46,16 +71,54 @@ export default function SignIn() {
   return currentUser.user ? (
     <h1>already signed in as {currentUser.user.username}</h1>
   ) : (
-    <form onSubmit={onSubmit}>
-      <label>
-        username
-        <input type="text" value={username} onChange={onUsernameChange} />
-      </label>
-      <label>
-        password
-        <input type="password" value={password} onChange={onPasswordChange} />
-      </label>
-      <input type="submit" />
-    </form>
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      className={classes.root}
+    >
+      <Paper className={classes.logInPaper}>
+        <form onSubmit={onSubmit}>
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <TextField
+              className={classes.textField}
+              required
+              label="username"
+              type="text"
+              value={username}
+              variant="outlined"
+              onChange={onUsernameChange}
+            />
+            <TextField
+              required
+              className={classes.textField}
+              label="password"
+              type="password"
+              value={password}
+              variant="outlined"
+              onChange={onPasswordChange}
+            />
+            <Button
+              variant="contained"
+              type="submit"
+              className={classes.button}
+            >
+              log in
+            </Button>
+          </Grid>
+        </form>
+      </Paper>
+    </Grid>
   );
 }
