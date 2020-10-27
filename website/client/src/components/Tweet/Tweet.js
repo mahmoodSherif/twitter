3e21968f95f564f46b59623dc1c3ab7bf7b2b6c6
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-10-27 22:52:41 +0200

    add style to Tweet

--- website/client/src/components/Tweet/Tweet.js
+++ website/client/src/components/Tweet/Tweet.js
@@ -1,16 +1,60 @@
-import React from "react";
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Collapse,
+  IconButton,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
+import CommentIcon from "@material-ui/icons/Comment";
+import React, { useState } from "react";
 import "./tweet.css";
 
 export default function Tweet(props) {
+  const [liked, setLiked] = useState(props.tweet.liked | false);
+  const [expanded, setExpanded] = useState(false);
+
+  function handleExpandClick() {
+    setExpanded(!expanded);
+  }
+
+  function handleLikeClick() {
+    setLiked(!liked);
+  }
   return (
-    <div className="tweet">
-      <img src={props.user.photourl} alt="user" className="avatar" />
-      <div className="naming">
-        <p className="nickname">{props.user.nickname}</p>
-        <p className="username">@{props.user.username}</p>
-      </div>
-      <p className="date">{new Date(props.tweet.createdat).toDateString()}</p>
-      <p>{props.tweet.text}</p>
-    </div>
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe">
+            <img src={props.user.photourl} alt="user" className="avatar" />
+          </Avatar>
+        }
+        title={props.user.nickname}
+        subheader={"@" + props.user.username}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text" component="p">
+          {props.tweet.text}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton aria-label="comment" onClick={handleLikeClick}>
+          {liked ? <FavoriteIcon /> : <FavoriteBorder />}
+        </IconButton>
+        <IconButton onClick={handleExpandClick} aria-label="comment">
+          <CommentIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <TextField></TextField>
+        </CardContent>
+      </Collapse>
+    </Card>
   );
 }
