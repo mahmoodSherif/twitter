674bcc9a0e16c0aa8a5ef920c512b7a88996c0ba
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-11-02 01:52:58 +0200

    use fetchData hook

--- website/client/src/components/UserProfile/index.js
+++ website/client/src/components/UserProfile/index.js
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useTweetsByUser } from "../../actions/tweets";
-import { useUserData } from "../../actions/user";
-import { useUserFollowed } from "../../actions/user";
+import { useFetchData } from "../../actions/helper";
 import TweetForm from "../TweetForm/TweetForm";
 import TweetList from "../TweetList/TweetList";
 
 export default function UserProfile() {
   const { userId } = useParams();
-  const user = useUserData(userId);
-  // const followed = useUserFollowed(userId);
-  const tweets = useTweetsByUser(userId);
-  return user && tweets ? (
+  const [user, fetchUser] = useFetchData();
+  const [tweets, fetchTweets] = useFetchData();
+
+  useEffect(() => {
+    fetchUser({ method: "get", url: `/users/${userId}` });
+  }, [fetchUser, userId]);
+
+  useEffect(() => {
+    fetchTweets({ method: "get", url: `/users/${userId}/tweets` });
+  }, [fetchTweets, userId]);
+
+  return (
     <div>
-      <div>
-        <h3>{user.nickname}</h3>
-        <h3>@{user.username}</h3>
-        {/* <button>{followed}</button> */}
-      </div>
+      {user.data && (
+        <div>
+          <h3>{user.data.nickname}</h3>
+          <h3>@{user.data.username}</h3>
+          {/* <button>{followed}</button> */}
+        </div>
+      )}
       <TweetForm />
-      <TweetList tweets={tweets} />
+      {tweets.data && <TweetList tweets={tweets.data} />}
     </div>
-  ) : (
-    <h1>loading...</h1>
   );
 }
