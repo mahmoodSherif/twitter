ab5b9d631e6bc733adc675dc40958d525e5b19a6
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-11-02 01:27:47 +0200

    use fetchData hook and add loading

--- website/client/src/components/Feed/index.js
+++ website/client/src/components/Feed/index.js
@@ -1,14 +1,38 @@
-import React from "react";
-import { useFeed } from "../../actions/tweets";
+import { CircularProgress, Grid, makeStyles } from "@material-ui/core";
+import React, { useContext, useEffect } from "react";
+import { useFetchData } from "../../actions/helper";
+import { AuthContext } from "../../contexts/auth";
 import TweetForm from "../TweetForm/TweetForm";
 import TweetList from "../TweetList/TweetList";
 
+const useStyles = makeStyles({
+  circularProgress: {
+    padding: "10px",
+  },
+});
+
 export default function Feed() {
-  const tweets = useFeed();
+  const { currentUser } = useContext(AuthContext);
+  const [tweets, fetch] = useFetchData();
+  const classes = useStyles();
+
+  useEffect(() => {
+    fetch({
+      url: `/feed/`,
+      method: "get",
+    });
+  }, [currentUser.user.id, fetch]);
+
   return (
     <div>
       <TweetForm />
-      {tweets ? <TweetList tweets={tweets} /> : <h1>Loading...</h1>}
+      {tweets.isLoading && (
+        <Grid container alignItems="center" justify="center">
+          <CircularProgress className={classes.circularProgress} />
+        </Grid>
+      )}
+      {tweets.data && <TweetList tweets={tweets.data} />}
+      {tweets.error && <h1>ERROR</h1>}
     </div>
   );
 }
